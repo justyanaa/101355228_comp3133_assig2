42d088248f8ffd8fa927fb6442ce01049365f3ee
Iana Bosenko <112834476+justyanaa@users.noreply.github.com>
2024-04-07 20:27:42 -0400

    Update graphql.provider.ts

--- src/app/graphql.provider.ts
+++ src/app/graphql.provider.ts
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-angular/http';
 import { ApplicationConfig, inject } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-const uri = 'http://localhost:4000/'; // <-- add the URL of the GraphQL server here
+const uri = 'https://backend-comp3133.onrender.com'; // <-- add the URL of the GraphQL server here
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
   return {
